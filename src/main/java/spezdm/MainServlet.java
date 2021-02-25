14fecef5083ba35f8c0a2829c8065d6573b0fd78
spezdmtest <spezd.m.@gmail.com>
2021-02-25 17:01:20 +0200

    servletAPI

---
+++ src/main/java/spezdm/MainServlet.java
@@ -0,0 +1,62 @@
+package spezdm;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+@WebServlet(urlPatterns = {"/cool-servlet","/my-cool-servlet/*"})
+public class MainServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        log("Method init =)");
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Method service enter\n");
+        super.service(req, resp);
+        resp.getWriter().write("Method service exit\n");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       String uri = req.getRequestURI();
+       String params = formatParams(req);
+
+       resp.getWriter().write("Method doGet\nURI: " + uri + "\nParams:\n" + params + "\n");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String uri = req.getRequestURI();
+        String params = formatParams(req);
+
+        resp.getWriter().write("Method doPost\nURI: " + uri + "\nParams:\n" + params + "\n");
+    }
+
+    private String formatParams(HttpServletRequest req) {
+        return req.getParameterMap()
+                .entrySet()
+                .stream()
+                .map(entry -> {
+                    String param = String.join(" and ", entry.getValue());
+                    return entry.getKey() + " => " + param;
+                } )
+                  .collect(Collectors.joining("\n"));
+    }
+
+    @Override
+    public void destroy() {
+        log("Method destroy =) ");
+
+    }
+}
